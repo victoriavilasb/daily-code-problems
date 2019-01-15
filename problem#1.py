1b23b0b6b547207a3613363a9e3274ee7dc0b60f
victoriavilasb <vick_vilasb@outlook.com>
2019-01-14 23:42:40 -0200

    adding the problem description

--- problem#1.py
+++ problem#1.py
@@ -1,3 +1,16 @@
+''''
+Good morning! Here's your coding interview problem for today.
+This problem was recently asked by Google.
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+Bonus: Can you do this in one pass?
+Upgrade to premium and get in-depth solutions to every problem.
+If you liked this problem, feel free to forward it along so they can subscribe here! As always, shoot us an email if there's anything we can help with!
+''''
+
+
+
+
 def find_sum(list_e, k):
   for i in range(0,len(list_e)-1):
     for j in range(i+1,len(list_e)):
